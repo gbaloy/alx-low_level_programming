28059c63d69cc4dee45660b5629624329c50ec4e
Godfrey Baloyi <godfreyatt@outlook.com>
2022-06-05 14:34:58 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void) {
+
+    for(int i=0; i<10; i++){
+    printf("%i", i);
+	}
+
+    return 0;
+}
