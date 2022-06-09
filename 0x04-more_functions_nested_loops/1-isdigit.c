ebc8a0b6a4279d612fa8c72df48ad004025ba9f1
Godfrey Baloyi <godfreyatt@outlook.com>
2022-06-09 12:50:21 -0700

    moe functios

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isupper - define if a character is uppercase
- *
- * Description: Prints the alphabet with _putchar
- * @c: charater
- * Return: void
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
