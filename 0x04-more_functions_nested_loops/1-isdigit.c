cd331154ac18a2c7ea3d5241bed53757886e5cc8
Godfrey Baloyi <godfreyatt@outlook.com>
2022-06-09 12:03:51 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	for (c = 48; c <= 59; c++)
+	{
+		return (1);
+	}
+
+	return (0);
+	return (c >= 48 && c <= 57);
+}
