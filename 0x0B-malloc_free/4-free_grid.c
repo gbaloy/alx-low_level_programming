412cd15dae9977130a5232f32f3bff65a58f1fa0
Godfrey Baloyi <godfreyatt@outlook.com>
2022-06-28 13:09:51 -0700

    2dimension

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,21 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* free_grid - 2d array
-*@grid:memory block 
-*@height:height 
-*Return:returns void
-*/
-
+ * free_grid - Entry point
+ *@grid: rows
+ *@height: columns
+ * Return: 2d 
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
+	int *p;
 
 	for (i = 0; i < height; i++)
-		free(grid[i]);
-
+	{
+		p = grid[i];
+		free(p);
+	}
 	free(grid);
 }
