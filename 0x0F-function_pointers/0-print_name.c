25d7553c2b26e3e30b3491bb0c2d33e8404cce69
Godfrey Baloyi <godfreyatt@outlook.com>
2022-07-07 12:37:53 -0700

    print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function to print name
+ * print_name - function that prints a name
  * @name: variable name
  * @f: void function
  */
 
-void print_name(char *name, void (*f)(char*)
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
 		f(name);
-
 	}
 }
-
