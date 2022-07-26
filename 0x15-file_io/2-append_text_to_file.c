68b7cdf6e616dbb654ae7940a328f845227f5340
Godfrey Baloyi <godfreyatt@outlook.com>
2022-07-25 20:10:59 -0700

    append file to text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "mainn.h"
 
 /**
  * append_text_to_file - that appends text at the end of a file
@@ -32,5 +32,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	write(file, text_content, i);
+
+	return (1);
 }
-	
